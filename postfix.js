4d3bf755bc2f9cdc69332f0b45b7c4ef0a7652a2
mortie <martid0311@gmail.com>
2014-04-12 12:28:01 +0200

    fundamentals working

--- postfix.js
+++ postfix.js
@@ -1,32 +1,68 @@
-var operators = {
-	"+": function() {
-		stack.push(stack.pop()+stack.pop());
-	},
-	"-": function() {
-		stack.push(stack.pop()-stack.pop());
-	},
-	"*": function() {
-		stack.push(stack.pop()*stack.pop());
-	},
-	"/": function() {
-		stack.push(stack.pop()/stack.pop());
-	},
-	"%": function() {
-		stack.push(stack.pop()%stack.pop());
-	},
-
-	"def": function() {
-		
-	}
-}
-
-var stack = [];
-
 function evaluate(str) {
-	var tokens = str.split(/\s/);
+	var operators = {
+		"+": function() {
+			stack.push(stack.pop()+stack.pop());
+		},
+		"-": function() {
+			stack.push(stack.pop()-stack.pop());
+		},
+		"*": function() {
+			stack.push(stack.pop()*stack.pop());
+		},
+		"/": function() {
+			stack.push(stack.pop()/stack.pop());
+		},
+		"%": function() {
+			stack.push(stack.pop()%stack.pop());
+		},
 
-	for (var i in tokens) {
-		var token = tokens[i];
+		"def": function() {
+			variables[stack.pop()] = stack.pop();
+		},
+		"get": function() {
+			stack.push(variables[stack.pop()]);
+		},
+		"exec": function() {
+			var ret = (evaluate(stack.pop()));
+			var i;
+
+			for (i=0; i<ret.length; ++i) {
+				stack.push(ret[i]);
+			}
+		},
+
+		"{": function() {
+			var result = "";
+			var depth = 0;
+			var i;
+
+			for (i=tokenIndex+1; i<tokens.length; ++i) {
+				token = tokens[i];
+
+				if (token == "{") {
+					++depth;
+				} else if (token == "}") {
+					if (depth <= 0) {
+						break;
+					} else {
+						--depth;
+					}
+				}
+				result += token+" ";
+			}
+
+			tokenIndex = i;
+			stack.push(result.slice(0, -1));
+		}
+	}
+
+	var stack = [];
+	var variables = [];
+	var tokens = str.split(/\s/);
+	var tokenIndex;
+
+	for (tokenIndex=0; tokenIndex<tokens.length; ++tokenIndex) {
+		var token = tokens[tokenIndex];
 
 		if (token.match(/[0-9\.]/)) {
 			stack.push(token);
@@ -37,8 +73,8 @@ function evaluate(str) {
 		}
 	}
 
-	console.log(stack);
+	return stack;
 }
 
-evaluate("/hello");
+console.log(evaluate("{ 100 10 * } exec"));
 
